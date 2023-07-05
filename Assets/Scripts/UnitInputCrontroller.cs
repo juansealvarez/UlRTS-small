8034167c0bb85b47a6b25d4721c80bd7945ab935
A16213 <A16213@lab.aca>
2023-07-05 17:46:35 -0500

    add multiple selection

--- Assets/Scripts/UnitInputCrontroller.cs
+++ Assets/Scripts/UnitInputCrontroller.cs
@@ -17,6 +17,11 @@ public class UnitInputCrontroller : MonoBehaviour
     public event UnityAction OnCancelSelection;
 
     public static UnitInputCrontroller Instance { get; private set; }
+    private bool mPressed = false;
+    private Vector3 mMultipleSelectionStartPosition;
+    private Vector3 mMultipleSelectionEndPosition;
+    private Vector3 center;
+    private Vector3 halfExtends;
 
     private void Awake() {
         Instance = this;
@@ -81,6 +86,77 @@ public class UnitInputCrontroller : MonoBehaviour
                 // Mover las unidades
                 UnitManager.Instance.MoveUnits(hit.point);
             }
+        }
+    }
+
+    private void OnMultipleSelectionPress(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(
+                Mouse.current.position.x.value,
+                Mouse.current.position.y.value
+            ));
+
+            if (Physics.Raycast(
+                ray,
+                out RaycastHit hit,
+                float.MaxValue
+            ))
+            {
+                mMultipleSelectionStartPosition = hit.point;
+                mMultipleSelectionEndPosition = mMultipleSelectionStartPosition;
+                mPressed = true;
+            }
+        }
+    }
+
+    private void OnMultipleSelectionRelease(InputValue value)
+    {
+        if (mPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(
+                Mouse.current.position.x.value,
+                Mouse.current.position.y.value
+            ));
+
+            if (Physics.Raycast(
+                ray,
+                out RaycastHit hit,
+                float.MaxValue
+            ))
+            {
+                mMultipleSelectionEndPosition = hit.point;
+
+                center = new Vector3(
+                    (mMultipleSelectionStartPosition.x + mMultipleSelectionEndPosition.x)/2f,
+                    0f,
+                    (mMultipleSelectionStartPosition.z + mMultipleSelectionEndPosition.z)/2f
+                );
+
+                halfExtends = new Vector3(
+                    Mathf.Abs(mMultipleSelectionEndPosition.x - mMultipleSelectionStartPosition.x)/2,
+                    1f,
+                    Mathf.Abs(mMultipleSelectionEndPosition.z - mMultipleSelectionStartPosition.z)/2
+                );
+
+                var colliders = Physics.OverlapBox(
+                    center,
+                    halfExtends,
+                    Quaternion.identity,
+                    LayerMask.GetMask("Unit")
+                );
+
+                foreach (var collider in colliders)
+                {
+                    if (collider.TryGetComponent<Unit>(out Unit unit))
+                    {
+                        UnitManager.Instance.SelectUnit(unit);
+                        mPressed = false;
+                    }
+                }
+            }
+            
         }
     }
 }
